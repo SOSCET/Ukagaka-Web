01c4c777d607a1b80b2a291a70847bad323c8daf
excfergodst <excfergodst@gmail.com>
2015-09-27 07:08:17 +0000

    20150926 in fateyan house

---
+++ FrontEnd/Comunacation.js
@@ -0,0 +1,30 @@
+class Message 
+{
+   
+    
+    Connect(ip,port)
+    { 
+       let Successfully ;
+        //Connect Code    
+        return Successfully ; 
+    }
+    
+    SendMsg(msg)
+    {
+        this.msg = msg;
+        //Send MSG code        
+    }
+    
+    RecvMsg()
+    {
+        let msg;
+        //Recvice Message Code
+        return msg;    
+    }
+
+}
+
+
+
+
+
